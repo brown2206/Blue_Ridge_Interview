54b6f7b9436dba477fb5832fe433f15899e476c4
brown2206 <brown.angelo.jr@gmail.com>
2017-12-16 17:19:48 -0500

    migrate and seed database

--- db/migrate/20171216211309_create_candidates.rb
+++ db/migrate/20171216211309_create_candidates.rb
@@ -1,6 +1,12 @@
 class CreateCandidates < ActiveRecord::Migration[5.0]
   def change
     create_table :candidates do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :email
+      t.string :gender
+      t.string :ip_address
+      t.date :date_of_birth
     end
   end
 end
