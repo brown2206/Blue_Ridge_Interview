5343ee1f81c8559a9b6a09cdc3a94281959bdfe9
brown2206 <brown.angelo.jr@gmail.com>
2017-12-17 02:15:57 -0500

    worked on views

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -2,4 +2,12 @@ class CandidatesController < ApplicationController
     def index
       @candidates = Candidate.all
     end
+
+    def new
+      @candidate = Candidate.new
+    end
+
+    def show
+      @candidate = Candidate.find(params[:id])
+    end
 end
