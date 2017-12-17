5ccd3136894a18eb913f5a3825a4dafe457240fb
brown2206 <brown.angelo.jr@gmail.com>
2017-12-16 21:03:50 -0500

    fixing database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,12 @@ require 'csv'
 csv_text = File.read(Rails.root.join('lib', 'seeds', 'interview_sample_data_01.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
 csv.each do |row|
-  puts row.to_hash
+  Candidate.create! do |model|
+    model.first_name = row[1]
+    model.last_name = row[2]
+    model.email = row[3]
+    model.gender = row[4]
+    model.ip_address = row[5]
+    model.date_of_birth = row[6]
+  end
 end
