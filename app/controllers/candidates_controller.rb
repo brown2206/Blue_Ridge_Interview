7905459c72c05da22c7229f92e0984edd22b8ec3
brown2206 <brown.angelo.jr@gmail.com>
2017-12-17 03:08:49 -0500

    implemented full CRUD

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -7,7 +7,33 @@ class CandidatesController < ApplicationController
       @candidate = Candidate.new
     end
 
+    def create
+      @candidate = Candidate.create!(candidate_params)
+      redirect_to candidate_path(@candidate)
+    end
+
     def show
       @candidate = Candidate.find(params[:id])
+    end
+
+    def edit
+      @candidate = Candidate.find(params[:id])
+    end
+
+    def update
+      @candidate = Candidate.find(params[:id])
+      @candidate.update(candidate_params)
+      redirect_to candidate_path(@candidate)
+    end
+
+    def destroy
+      @candidate = Candidate.find(params[:id])
+      @candidate.destroy
+      redirect_to candidates_path
+    end
+
+    private
+    def candidate_params
+      params.require(:candidate).permit(:first_name, :last_name, :email, :gender, :ip_address, :date_of_birth)
     end
 end
